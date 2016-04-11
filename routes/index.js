76f0b2181b72018f199b5efc4aca1da45c6a48fb
Donna Yee <dhyee@ucsd.edu>
2016-04-11 11:25:06 -0700

    Finished Part 1.2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-// var models = require("../models");
+var models = require("../models");
 
 exports.view = function(req, res) {
-    /* TODO */
+	var data = {data: []};
+    res.render("index", data);
 };
--- server.js
+++ server.js
@@ -9,19 +9,39 @@ const MongoStore = require("connect-mongo")(session);
 
 require("dotenv").load();
 var models = require("./models");
+var mongoose = require('mongoose');
 var db = mongoose.connection;
 
-var router = { /* TODO */};
+var handlebars = require('express-handlebars');
+var passport = require("passport");
+
+var router = { 
+	index: require('./routes/index'),
+    chat: require('./routes/chat'),
+    twitter: passport.authenticate('twitter'),
+    callback: passport.authenticate('twitter', { successRedirect: '/chat', failureRedirect: '/'}),
+    logout: function(req, res){
+                req.logout();
+                res.redirect('/');
+    }
+};
 
 var parser = {
     body: require("body-parser"),
     cookie: require("cookie-parser")
 };
 
-var strategy = { /* TODO */ };
+var strategy = { 
+    "Twitter": require("passport-twitter").Strategy 
+};
 
 // Database Connection
-/* TODO */
+var db = mongoose.connection;
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/cogs121');
+db.on('error', console.error.bind(console, 'Mongo DB Connection Error:'));
+db.once('open', function(callback) {
+    console.log("Database connected successfully.");
+});
 
 // session middleware
 var session_middleware = session({
@@ -44,16 +64,71 @@ app.use(parser.body.json());
 app.use(require('method-override')());
 app.use(session_middleware);
 /* TODO: Passport Middleware Here*/
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* TODO: Use Twitter Strategy for Passport here */
+passport.use(new strategy.Twitter({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "/auth/twitter/callback"
+}, function(token, token_secret, profile, done) {
+    models.User.findOne({ "twitterID": profile.id }, function(err, user) {
+    // (1) Check if there is an error. If so, return done(err);
+    if(err) {
+    	return done(err);
+    }
+    if(!user) {
+        // (2) since the user is not found, create new user.
+        // Refer to Assignment 0 to how create a new instance of a model
+        var newUser = new models.User({
+			"twitterID": profile.id,
+			"token": token,
+			"username": profile.username,
+			"displayName": profile.displayName,
+			"photo": null
+		});
+		newUser.save(afterSaving);
+
+		function afterSaving(err, user){
+			if(err) { 
+				console.log(err);
+				res.send(500);
+			}
+		}
+        return done(null, profile);
+    } else {
+        // (3) since the user is found, update user’s information
+        process.nextTick(function() {
+        	user.twitterID = profile.id;
+        	user.token = token;
+        	user.username = profile.username;
+        	user.displayName = profile.displayName;
+        	user.photo = profile.photos[0];
+        	user.save();
+
+            return done(null, profile);
+        });
+    }
+  });
+}));
 
 /* TODO: Passport serialization here */
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 // Routes
 /* TODO: Routes for OAuth using Passport */
-// app.get("/", router.index.view);
+app.get("/", router.index.view);
+app.get("/chat", router.chat.view);
 // More routes here if needed
-
+app.get('/auth/twitter', router.twitter);
+app.get('/auth/twitter/callback', router.callback);
+app.get('/logout', router.logout);
 // io.use(function(socket, next) {
 //     session_middleware(socket.request, {}, next);
 // });
